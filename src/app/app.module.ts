f46c698eb2b0ff6d0f51d329fe07a8d266bc0bab
abdikeem <abdihakimabdirahman21@gmail.com>
2019-12-04 11:39:10 +0300

    add component not-found

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { NgProgressModule } from '@ngx-progressbar/core';
 import { NgProgressHttpClientModule } from '@ngx-progressbar/http-client';
 import { AboutComponent } from './about/about.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 // import { GoalService } from './goal-service/goal.service';
 
 @NgModule({
@@ -26,6 +27,7 @@ import { NavbarComponent } from './navbar/navbar.component';
     GoalFormComponent,
     AboutComponent,
     NavbarComponent,
+    NotFoundComponent,
     // GoalService,
     
   ],
